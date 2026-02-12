6b7c3439644bcbdf8947dfa15b235c1f5ecff923
Alexia Cervantes franco <alexiacervantesfranco@MacBook-Air-de-Alexia.local>
2026-02-12 08:40:43 -0700

    cambios practica4

---
+++ app/src/main/java/mx/edu/itesca/practica4/MainActivity.kt
@@ -0,0 +1,31 @@
+package mx.edu.itesca.practica4
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class   MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Ajuste de insets (edge-to-edge)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Click del botón para ir a RegisterActivity
+        val button: Button = findViewById(R.id.btn_get_started)
+        button.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
